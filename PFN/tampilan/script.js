58b438f3f85132a681f72edb13d9d14e3c88a104
unknown <4520210085@univpancasila.ac.id>
2023-05-18 21:39:33 +0700

    Portofolio PFN

--- PFN/tampilan/script.js
+++ PFN/tampilan/script.js
@@ -69,7 +69,16 @@ Vue.component('card', {
       }, 1000);
     } } });
 
-
+    $(".custom-carousel").owlCarousel({
+      autoWidth: true,
+      loop: true
+    });
+    $(document).ready(function () {
+      $(".custom-carousel .item").hover(function () {
+        $(".custom-carousel .item").not($(this)).removeClass("active");
+        $(this).toggleClass("active");
+      });
+    });
 
 const app = new Vue({
   el: '#app' });
